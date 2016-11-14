2a5c19cfeec4d32b15fdd78cf75dd5ebc637ae83
ExcaliburZhou <1293556249@qq.com>
2016-11-14 18:35:57 +0800

    update

---
+++ public/data.js
@@ -0,0 +1,78 @@
+/**
+ * Created by Excalibur on 16/11/14.
+ */
+export default {
+    "code": 200,
+    "message": "",
+    "data": {
+        "pageTotal": 2,
+        "pageSize": 10,
+        "page": 1,
+        "list": [{
+            "type": 6,
+            "createTime": "2015-12-21 08:33:23",
+            "title": "标题5",
+            "desc": "大家所说的路上看到就拉开圣诞节,打开撒旦恐怕我恐怕.的那我那的 打开电脑就能去,第五代歼击机.",
+            "img": ["https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg", "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg"],
+            "detail": {
+                "fee": 1223.33,
+                "useAccount": "支付宝(fucku@wechat.com)",
+                "timeType": "提现时间",
+                "timeValue": "2015-12-11 12:33",
+                "withDrawId": "adkdkdkdkadf"
+            }
+        }, {
+            "type": 5,
+            "createTime": "2015-12-21 08:33:23",
+            "title": "标题5",
+            "desc": "大家所说的路上看到就拉开圣诞节,打开撒旦恐怕我恐怕.的那我那的 打开电脑就能去,第五代歼击机.",
+            "img": ["https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg", "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg"],
+            "detail": {
+                "fee": 1223.33,
+                "useAccount": "支付宝(fucku@wechat.com)",
+                "timeType": "提现时间",
+                "timeValue": "2015-12-11 12:33",
+                "withDrawId": "adkdkdkdkadf"
+            }
+        }, {
+            "type": 4,
+            "createTime": "2015-12-21 08:33:23",
+            "desc": "大家所说的路上看到就拉开圣诞节,打开撒旦恐怕我恐怕.的那我那的 打开电脑就能去,第五代歼击机.",
+            "title": "标题5",
+            "img": ["https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg", "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg"],
+            "detail": {
+                "fee": 1223.33,
+                "useAccount": "支付宝(fucku@wechat.com)",
+                "timeType": "提现时间",
+                "timeValue": "2015-12-11 12:33",
+                "withDrawId": "adkdkdkdkadf"
+            }
+        }, {
+            "type": 3,
+            "createTime": "2015-12-21 08:33:23",
+            "desc": "大家所说的路上看到就拉开圣诞节,打开撒旦恐怕我恐怕.的那我那的 打开电脑就能去,第五代歼击机.",
+            "title": "标题5",
+            "img": ["https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg", "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg"],
+            "detail": {
+                "fee": 1223.33,
+                "useAccount": "支付宝(fucku@wechat.com)",
+                "timeType": "提现时间",
+                "timeValue": "2015-12-11 12:33",
+                "withDrawId": "adkdkdkdkadf"
+            }
+        }, {
+            "type": 2,
+            "createTime": "2015-12-21 08:33:23",
+            "desc": "大家所说的路上看到就拉开圣诞节,打开撒旦恐怕我恐怕.的那我那的 打开电脑就能去,第五代歼击机.",
+            "title": "标题5",
+            "img": ["https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg", "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D200/sign=af9259bf03082838770ddb148898a964/6159252dd42a2834bc76c4ab5fb5c9ea14cebfba.jpg"],
+            "detail": {
+                "fee": 1223.33,
+                "useAccount": "支付宝(fucku@wechat.com)",
+                "timeType": "提现时间",
+                "timeValue": "2015-12-11 12:33",
+                "withDrawId": "adkdkdkdkadf"
+            }
+        }]
+    }
+}
